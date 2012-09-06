9d3b237161cc10b7c6ab52bd0485848ac36d8f7e
Tomas M <tomas@slax.org>
2012-09-05 23:43:33 -0500

    make bootinst script universal for LiveKit, not slax specific

--- bootfiles/bootinst.bat
+++ bootfiles/bootinst.bat
@@ -1,14 +1,7 @@
 @echo off
 cls
 echo ===============================================================================
-echo.
-echo                            ________.__
-echo                           /   ____/^|  ^| _____  ___  ___
-echo                           \____  \ ^|  ^| \__  \ \  \/  /
-echo                           /       \^|  ^|__/ __ \_^>    ^<
-echo                          /______  /^|____(____  /__/\_ \
-echo                                 \/           \/      \/
-echo.
+echo                           Setting your drive to boot
 echo ===============================================================================
 echo.
 
@@ -18,8 +11,8 @@ set BOOTFLAG=boot666s.tmp
 echo This file is used to determine current drive letter. It should be deleted. >\%BOOTFLAG%
 if not exist \%BOOTFLAG% goto readOnly
 
-echo.|set /p=wait please 
-for %%d in ( C D E F G H I J K L M N O P Q R S T U V W X Y Z ) do echo.|set /p=. & if exist %%d:\%BOOTFLAG% set DISK=%%d
+echo wait please ...
+for %%d in ( C D E F G H I J K L M N O P Q R S T U V W X Y Z ) do if exist %%d:\%BOOTFLAG% set DISK=%%d
 del \%BOOTFLAG%
 if %DISK% == none goto DiskNotFound
 
@@ -41,7 +34,7 @@ echo Installation finished.
 goto pauseit
 
 :readOnly
-echo You're starting Slax installer from a read-only media, this will not work.
+echo You're starting boot installer from a read-only media, this will not work.
 goto pauseit
 
 :DiskNotFound
