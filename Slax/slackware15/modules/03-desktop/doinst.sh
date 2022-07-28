4099a2e730c1aaca3512a9985bfca1c2fa533508
Tomas M <tomas@slax.org>
2022-07-27 20:31:52 -0800

    Slackware testing

---
+++ Slax/slackware15/modules/03-desktop/doinst.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# $1 = union directory
+chown -R guest.users $1/home/guest
---
+++ Slax/slackware15/modules/04-apps/doinst.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+UNION=$1
+SLACKWARE=$2
+
+TMP=/tmp/gcc-root$$
+installpkg -root $TMP $SLACKWARE/d/gcc-11*.txz
+cp -a $TMP/usr/lib64/libquadmath.so.0* $UNION/usr/lib64
+rm -Rf $TMP
