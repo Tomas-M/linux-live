2228261bd7d8805dc7bbbc150e02f323e0d3a6d7
Tomas M <tomas@slax.org>
2022-07-31 19:26:53 +0000

    slackware testing

--- Slax/slackware15/modules/04-apps/doinst.sh
+++ Slax/slackware15/modules/04-apps/doinst.sh
@@ -7,3 +7,5 @@ TMP=/tmp/gcc-root$$
 installpkg -root $TMP $SLACKWARE/d/gcc-11*.txz
 cp -a $TMP/usr/lib64/libquadmath.so.0* $UNION/usr/lib64
 rm -Rf $TMP
+
+glib-compile-schemas $1/usr/share/glib-2.0/schemas/
