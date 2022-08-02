692d8dec89ba64943164ea9f73427a4dd1b9833a
Tomas M <tomas@slax.org>
2022-08-02 08:14:03 +0000

    slackware testing

--- Slax/slackware15/helpers/fw.php
+++ Slax/slackware15/helpers/fw.php
@@ -2,12 +2,13 @@
 
    // There are quadrilions of different firmwares, can't possibly include all in Slax
    // We're going to delete all firmware drivers which are not wifi related
-   // Run this script, it is safe (read only). Prints drivers which should be deleted
+   // Run this script, it is safe (read only). Prints drivers which are wifi-related
 
    $result=array();
    $fwdir="/lib/firmware";
+   chdir($fwdir);
 
-   echo "# Firmware files for deletion:\n";
+   echo "# Firmware files for inclusion:\n";
 
    $descr=file_get_contents("$fwdir/WHENCE");
    $drivers=preg_split("{---------------+}",$descr);
@@ -16,23 +17,22 @@
    {
       if (preg_match("{Driver:(.*)}i",$bunch,$matches))
       {
-         $size=0; $files=array();
+         $files=array();
          $driver=trim($matches[1]);
          preg_match_all("{(File|Source):(.*)}i",$bunch,$matches);
          foreach($matches[2] as $file)
          {
             $file=$fwdir."/".trim($file);
-            $files[]=$file;
-            $size+=filesize($file);
+            if (@filesize($file)>0) $files[]=$file;
          }
-         if (preg_match("{wifi|wireless|wi-fi|radeon|atheros}i",$bunch))
-            if (!preg_match("{audio|bluetooth|serial|adsl|wimax|USB}i",$bunch))
+         if (preg_match("{wifi|wireless|wi-fi|radeon|atheros|802}i",$bunch))
+            if (!preg_match("{amdgpu|audio|bluetooth|serial|adsl|wimax|USB}i",$bunch))
             {
-               $result[$driver]=floor($size/1024);
+               $result+=$files;
             }
-         else echo join("\n",$files)."\n";
-         else echo join("\n",$files)."\n";
       }
    }
+
+   foreach($result as $row) echo "mkdir -p \$1/".dirname($row)."\ncp $row \$1".$row."\n";
 
 ?>
