e798a3ed04fda7cc4e8d61ee8d87f25e1d472346
Tomas M <tomas@slax.org>
2023-01-08 22:16:32 +0000

    upd version

--- Slax/slackware15/modules/04-apps/doinst.sh
+++ Slax/slackware15/modules/04-apps/doinst.sh
@@ -4,7 +4,7 @@ UNION=$1
 SLACKWARE=$2
 
 TMP=/tmp/gcc-root$$
-installpkg -root $TMP $SLACKWARE/d/gcc-11*.txz
+installpkg -root $TMP $SLACKWARE/d/gcc-12*.txz
 cp -a $TMP/usr/lib64/libquadmath.so.0* $UNION/usr/lib64
 rm -Rf $TMP
 
