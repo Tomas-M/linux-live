3b9d88895f4b7904ac06eec372cb5786b655557d
TomasM <tomas@slax.org>
2023-01-09 20:31:07 +0000

    quadmath lib for galculator

--- Slax/slackware15/modules/04-apps/doinst.sh
+++ Slax/slackware15/modules/04-apps/doinst.sh
@@ -5,7 +5,14 @@ SLACKWARE=$2
 
 TMP=/tmp/gcc-root$$
 installpkg -root $TMP $SLACKWARE/d/gcc-12*.txz
-cp -a $TMP/usr/lib64/libquadmath.so.0* $UNION/usr/lib64
+
+if [ "$(uname -m)" = "x86_64" ]; then
+   ARCH=64
+else
+   ARCH=""
+fi
+
+cp -a $TMP/usr/lib64/libquadmath.so.* $UNION/usr/lib$ARCH
 rm -Rf $TMP
 
 glib-compile-schemas $1/usr/share/glib-2.0/schemas/
